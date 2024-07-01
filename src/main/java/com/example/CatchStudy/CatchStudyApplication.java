4738af096c66f0746482507622c479ff93be3399
이다은 <llynn97@naver.com>
2024-07-01 12:06:04 +0900

    init : 초기 설정

---
+++ src/main/java/com/example/CatchStudy/CatchStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.CatchStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CatchStudy/CatchStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CatchStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
