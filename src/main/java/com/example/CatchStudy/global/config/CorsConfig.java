63858df6be2b345a1e8e0c65fb1b17e0962bbfbd
TaeHoon0 <kimtahwn@naver.com>
2024-07-10 11:51:16 +0900

    chore : cors config, yml 수정

--- src/main/java/com/example/CatchStudy/global/config/CorsConfig.java
+++ src/main/java/com/example/CatchStudy/global/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.CatchStudy.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,7 +11,8 @@ import java.util.ArrayList;
 
 @Configuration
 public class CorsConfig {
-
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -19,6 +21,8 @@ public class CorsConfig {
         // 리소스 허용할 URL 지정
         ArrayList<String> allowedOriginPatterns = new ArrayList<>();
         allowedOriginPatterns.add("http://localhost:8080");
+        allowedOriginPatterns.add("http://localhost:3000");
+        allowedOriginPatterns.add(redisHost+":6739");
         corsConfig.setAllowedOrigins(allowedOriginPatterns);
 
         corsConfig.addAllowedMethod("*");        // 허용 메소드 지정
