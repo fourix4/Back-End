63f2f42121580d52e4253f85a8fb6e92596c9190
TaeHoon0 <kimtahwn@naver.com>
2024-07-13 11:54:59 +0900

    Test : login 성공 시 redirect 추가

--- src/main/java/com/example/CatchStudy/global/oauth/AuthenticationSuccessHandler.java
+++ src/main/java/com/example/CatchStudy/global/oauth/AuthenticationSuccessHandler.java
@@ -48,6 +48,5 @@ public class AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccess
         response.setCharacterEncoding("UTF-8");
 
         response.getWriter().write(objectMapper.writeValueAsString(responseData));
-        //response.sendRedirect("http://localhost:3000/oauthkakao");
     }
 }
