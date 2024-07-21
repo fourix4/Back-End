75ce282d306fa8e1c9a453a1d141e8a7ba0ab314
TaeHoon0 <kimtahwn@naver.com>
2024-07-21 16:23:43 +0900

    fix : cors websocket 연결 추가, method 변경

--- src/main/java/com/example/CatchStudy/global/config/CorsConfig.java
+++ src/main/java/com/example/CatchStudy/global/config/CorsConfig.java
@@ -21,7 +21,7 @@ public class CorsConfig {
         // 리소스 허용할 URL 지정
         corsConfig.addAllowedOriginPattern("http://localhost:3000");
         corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")); // 허용 메소드 지정
-        corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "content-type", "X-CSRF-Token")); // 허용 header 지정
+        corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "content-type", "X-CSRF-Token", "withCredentials")); // 허용 header 지정
         corsConfig.setAllowCredentials(true); // 인증, 인가 정보 허용
 
         // 웹소켓 연결 허용
