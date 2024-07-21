52441dcbdb6be1cd507986c80d882963852eb34b
TaeHoon0 <kimtahwn@naver.com>
2024-07-21 16:14:28 +0900

    fix : cors websocket 연결 추가, method 변경

--- src/main/java/com/example/CatchStudy/global/config/CorsConfig.java
+++ src/main/java/com/example/CatchStudy/global/config/CorsConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
@@ -19,9 +20,16 @@ public class CorsConfig {
 
         // 리소스 허용할 URL 지정
         corsConfig.addAllowedOriginPattern("http://localhost:3000");
-        corsConfig.addAllowedMethod("*");        // 허용 메소드 지정
-        corsConfig.addAllowedHeader("*");        // 허용 header 지정
-        corsConfig.setAllowCredentials(true);    // 인증, 인가 정보 허용
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")); // 허용 메소드 지정
+        corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "content-type", "X-CSRF-Token")); // 허용 header 지정
+        corsConfig.setAllowCredentials(true); // 인증, 인가 정보 허용
+
+        // 웹소켓 연결 허용
+        corsConfig.addAllowedOriginPattern("http://localhost:8080");
+        corsConfig.addAllowedHeader("Sec-WebSocket-Extensions");
+        corsConfig.addAllowedHeader("Sec-WebSocket-Key");
+        corsConfig.addAllowedHeader("Sec-WebSocket-Protocol");
+        corsConfig.addAllowedHeader("Sec-WebSocket-Version");
 
         source.registerCorsConfiguration("/**", corsConfig);
 
