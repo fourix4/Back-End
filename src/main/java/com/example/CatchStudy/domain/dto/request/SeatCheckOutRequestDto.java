37784d6327deebd0c77c34a6e8111fb08bea9deb
이다은 <llynn97@naver.com>
2024-07-27 22:45:51 +0900

    [#151] fix : 예약 기능 수정

--- src/main/java/com/example/CatchStudy/domain/dto/request/SeatCheckOutRequestDto.java
+++ src/main/java/com/example/CatchStudy/domain/dto/request/SeatCheckOutRequestDto.java
@@ -1,14 +1,14 @@
 package com.example.CatchStudy.domain.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor
 public class SeatCheckOutRequestDto {
 
+    @JsonProperty("booking_id")
     private Long booking_id;
 
 }
