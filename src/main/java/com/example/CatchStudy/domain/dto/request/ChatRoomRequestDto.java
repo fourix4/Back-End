3c3f7c1a3cd2212f7acbdb9b42479193f168294c
TaeHoon0 <kimtahwn@naver.com>
2024-07-28 14:26:21 +0900

    [#148] refactor : manager 수정 로직 변경

--- src/main/java/com/example/CatchStudy/domain/dto/request/ChatRoomRequestDto.java
+++ src/main/java/com/example/CatchStudy/domain/dto/request/ChatRoomRequestDto.java
@@ -1,6 +1,5 @@
 package com.example.CatchStudy.domain.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
--- src/main/java/com/example/CatchStudy/domain/dto/request/RoomInfoRequestDto.java
+++ src/main/java/com/example/CatchStudy/domain/dto/request/RoomInfoRequestDto.java
@@ -2,10 +2,12 @@ package com.example.CatchStudy.domain.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class RoomInfoRequestDto {
 
