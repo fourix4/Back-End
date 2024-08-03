18da991109d51a59c825dc21e8097d7c7ba16cfc
TaeHoon0 <kimtahwn@naver.com>
2024-08-03 17:18:55 +0900

    [#248] refactor : GoogleProfile 기본 생성자 추가

--- src/main/java/com/example/CatchStudy/service/LoginService.java
+++ src/main/java/com/example/CatchStudy/service/LoginService.java
@@ -19,7 +19,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
